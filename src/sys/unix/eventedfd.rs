a4b73e06b6734be2fe837f3025a7fb3998cd642d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-01-16 15:30:36 +0100

    Cleanup of src/sys/unix/eventedfd.rs

--- src/sys/unix/eventedfd.rs
+++ src/sys/unix/eventedfd.rs
@@ -1,30 +1,28 @@
 use std::io;
 use std::os::unix::io::RawFd;
 
-use {Ready, Poll, PollOpt, Token};
 use event::Evented;
+use poll::{Poll, PollOpt, Ready, Token};
 
-/*
- *
- * ===== EventedFd =====
- *
- */
-
-#[derive(Debug)]
-
-/// Adapter for `RawFd` providing an [`Evented`] implementation.
+/// Adapter for a `RawFd` providing an [`Evented`] implementation.
 ///
-/// `EventedFd` enables registering any type with an FD with [`Poll`].
+/// `EventedFd` enables registering any type with an file descriptor with
+/// [`Poll`].
 ///
 /// While only implementations for TCP and UDP are provided, Mio supports
-/// registering any FD that can be registered with the underlying OS selector.
-/// `EventedFd` provides the necessary bridge.
+/// registering any file descriptor that can be registered with the underlying
+/// OS selector. `EventedFd` provides the necessary bridge.
 ///
-/// Note that `EventedFd` takes a `&RawFd`. This is because `EventedFd` **does
-/// not** take ownership of the FD. Specifically, it will not manage any
-/// lifecycle related operations, such as closing the FD on drop. It is expected
-/// that the `EventedFd` is constructed right before a call to
-/// [`Poll::register`]. See the examples for more detail.
+/// Note that `EventedFd` takes a reference to a `RawFd`. This is because
+/// `EventedFd` **does not** take ownership of the file descriptor.
+/// Specifically, it will not manage any lifecycle related operations, such as
+/// closing the file descriptor on drop. It is expected that the `EventedFd` is
+/// constructed right before a call to [`Poll.register`]. See the examples
+/// below for more detail.
+///
+/// [`Evented`]: ../event/trait.Evented.html
+/// [`Poll`]: ../struct.Poll.html
+/// [`Poll.register`]: ../struct.Poll.html#method.register
 ///
 /// # Examples
 ///
@@ -33,20 +31,20 @@ use event::Evented;
 /// ```
 /// # use std::error::Error;
 /// # fn try_main() -> Result<(), Box<Error>> {
-/// use mio::{Ready, Poll, PollOpt, Token};
+/// use std::net::TcpListener;
+/// use std::os::unix::io::AsRawFd;
+///
+/// use mio::event::Evented;
+/// use mio::poll::{Poll, PollOpt, Ready, Token};
 /// use mio::unix::EventedFd;
 ///
-/// use std::os::unix::io::AsRawFd;
-/// use std::net::TcpListener;
-///
-/// // Bind a std listener
+/// // Bind a listener from the standard library.
 /// let listener = TcpListener::bind("127.0.0.1:0")?;
 ///
 /// let mut poll = Poll::new()?;
 ///
-/// // Register the listener
-/// poll.register(&mut EventedFd(&listener.as_raw_fd()),
-///              Token(0), Ready::READABLE, PollOpt::EDGE)?;
+/// // Register the listener using `EventedFd`.
+/// poll.register(&mut EventedFd(&listener.as_raw_fd()), Token(0), Ready::READABLE, PollOpt::EDGE)?;
 /// #     Ok(())
 /// # }
 /// #
@@ -58,12 +56,12 @@ use event::Evented;
 /// Implementing `Evented` for a custom type backed by a `RawFd`.
 ///
 /// ```
-/// use mio::{Ready, Poll, PollOpt, Token};
-/// use mio::event::Evented;
-/// use mio::unix::EventedFd;
-///
-/// use std::os::unix::io::RawFd;
 /// use std::io;
+/// use std::os::unix::io::RawFd;
+///
+/// use mio::event::Evented;
+/// use mio::poll::{Poll, PollOpt, Ready, Token};
+/// use mio::unix::EventedFd;
 ///
 /// pub struct MyIo {
 ///     fd: RawFd,
@@ -83,10 +81,7 @@ use event::Evented;
 ///     }
 /// }
 /// ```
-///
-/// [`Evented`]: ../event/trait.Evented.html
-/// [`Poll`]: ../struct.Poll.html
-/// [`Poll::register`]: ../struct.Poll.html#method.register
+#[derive(Debug)]
 pub struct EventedFd<'a>(pub &'a RawFd);
 
 impl<'a> Evented for EventedFd<'a> {
