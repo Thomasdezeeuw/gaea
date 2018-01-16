eed77d27664d046dab368e4d1064b926208bb053
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-01-16 16:11:46 +0100

    Cleanup src/sys/unix/tcp.rs

--- src/sys/unix/tcp.rs
+++ src/sys/unix/tcp.rs
@@ -1,138 +1,127 @@
 use std::io::{self, Read, Write};
 use std::net::{self, SocketAddr};
-use std::os::unix::io::{RawFd, FromRawFd, IntoRawFd, AsRawFd};
+use std::os::unix::io::{RawFd, AsRawFd, FromRawFd, IntoRawFd};
 use std::time::Duration;
 
 use libc;
 use net2::TcpStreamExt;
 
-use {Ready, Poll, PollOpt, Token};
 use event::Evented;
-
+use poll::{Poll, PollOpt, Ready, Token};
 use sys::unix::eventedfd::EventedFd;
-use sys::unix::io::set_nonblock;
 
 #[derive(Debug)]
 pub struct TcpStream {
-    inner: net::TcpStream,
-}
-
-#[derive(Debug)]
-pub struct TcpListener {
-    inner: net::TcpListener,
+    stream: net::TcpStream,
 }
 
 impl TcpStream {
     pub fn connect(stream: net::TcpStream, addr: &SocketAddr) -> io::Result<TcpStream> {
-        set_nonblock(stream.as_raw_fd())?;
+        stream.set_nonblocking(true)?;
 
         match stream.connect(addr) {
-            Ok(..) => {}
-            Err(ref e) if e.raw_os_error() == Some(libc::EINPROGRESS) => {}
+            Ok(..) => {},
+            Err(ref e) if e.raw_os_error() == Some(libc::EINPROGRESS) => {},
             Err(e) => return Err(e),
         }
 
-        Ok(TcpStream {
-            inner: stream,
-        })
+        Ok(TcpStream { stream })
     }
 
     pub fn from_stream(stream: net::TcpStream) -> TcpStream {
-        TcpStream {
-            inner: stream,
-        }
+        TcpStream { stream }
     }
 
     pub fn peer_addr(&self) -> io::Result<SocketAddr> {
-        self.inner.peer_addr()
+        self.stream.peer_addr()
     }
 
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
-        self.inner.local_addr()
+        self.stream.local_addr()
     }
 
     pub fn shutdown(&self, how: net::Shutdown) -> io::Result<()> {
-        self.inner.shutdown(how)
+        self.stream.shutdown(how)
     }
 
     pub fn set_nodelay(&self, nodelay: bool) -> io::Result<()> {
-        self.inner.set_nodelay(nodelay)
+        self.stream.set_nodelay(nodelay)
     }
 
     pub fn nodelay(&self) -> io::Result<bool> {
-        self.inner.nodelay()
+        self.stream.nodelay()
     }
 
     pub fn set_recv_buffer_size(&self, size: usize) -> io::Result<()> {
-        self.inner.set_recv_buffer_size(size)
+        self.stream.set_recv_buffer_size(size)
     }
 
     pub fn recv_buffer_size(&self) -> io::Result<usize> {
-        self.inner.recv_buffer_size()
+        self.stream.recv_buffer_size()
     }
 
     pub fn set_send_buffer_size(&self, size: usize) -> io::Result<()> {
-        self.inner.set_send_buffer_size(size)
+        self.stream.set_send_buffer_size(size)
     }
 
     pub fn send_buffer_size(&self) -> io::Result<usize> {
-        self.inner.send_buffer_size()
+        self.stream.send_buffer_size()
     }
 
     pub fn set_keepalive(&self, keepalive: Option<Duration>) -> io::Result<()> {
-        self.inner.set_keepalive(keepalive)
+        self.stream.set_keepalive(keepalive)
     }
 
     pub fn keepalive(&self) -> io::Result<Option<Duration>> {
-        self.inner.keepalive()
+        self.stream.keepalive()
     }
 
     pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
-        self.inner.set_ttl(ttl)
+        self.stream.set_ttl(ttl)
     }
 
     pub fn ttl(&self) -> io::Result<u32> {
-        self.inner.ttl()
+        self.stream.ttl()
     }
 
     pub fn set_only_v6(&self, only_v6: bool) -> io::Result<()> {
-        self.inner.set_only_v6(only_v6)
+        self.stream.set_only_v6(only_v6)
     }
 
     pub fn only_v6(&self) -> io::Result<bool> {
-        self.inner.only_v6()
+        self.stream.only_v6()
     }
 
     pub fn set_linger(&self, dur: Option<Duration>) -> io::Result<()> {
-        self.inner.set_linger(dur)
+        self.stream.set_linger(dur)
     }
 
     pub fn linger(&self) -> io::Result<Option<Duration>> {
-        self.inner.linger()
+        self.stream.linger()
     }
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        self.inner.take_error()
+        self.stream.take_error()
     }
 
     pub fn peek(&self, buf: &mut [u8]) -> io::Result<usize> {
-        self.inner.peek(buf)
+        self.stream.peek(buf)
     }
 }
 
 impl<'a> Read for &'a TcpStream {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        (&self.inner).read(buf)
+        (&self.stream).read(buf)
     }
 }
 
 impl<'a> Write for &'a TcpStream {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        (&self.inner).write(buf)
+        (&self.stream).write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        (&self.inner).flush()
+        (&self.stream).flush()
     }
 }
 
@@ -155,59 +144,62 @@ impl Evented for TcpStream {
 impl FromRawFd for TcpStream {
     unsafe fn from_raw_fd(fd: RawFd) -> TcpStream {
         TcpStream {
-            inner: net::TcpStream::from_raw_fd(fd),
+            stream: net::TcpStream::from_raw_fd(fd),
         }
     }
 }
 
 impl IntoRawFd for TcpStream {
     fn into_raw_fd(self) -> RawFd {
-        self.inner.into_raw_fd()
+        self.stream.into_raw_fd()
     }
 }
 
 impl AsRawFd for TcpStream {
     fn as_raw_fd(&self) -> RawFd {
-        self.inner.as_raw_fd()
+        self.stream.as_raw_fd()
     }
 }
 
+#[derive(Debug)]
+pub struct TcpListener {
+    listener: net::TcpListener,
+}
+
 impl TcpListener {
-    pub fn new(inner: net::TcpListener, _addr: &SocketAddr) -> io::Result<TcpListener> {
-        set_nonblock(inner.as_raw_fd())?;
-        Ok(TcpListener {
-            inner: inner,
-        })
+    pub fn new(listener: net::TcpListener, _addr: &SocketAddr) -> io::Result<TcpListener> {
+        listener.set_nonblocking(true)?;
+        Ok(TcpListener { listener })
     }
 
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
-        self.inner.local_addr()
+        self.listener.local_addr()
     }
 
     pub fn accept(&self) -> io::Result<(net::TcpStream, SocketAddr)> {
-        self.inner.accept()
+        self.listener.accept()
     }
 
     #[allow(deprecated)]
     pub fn set_only_v6(&self, only_v6: bool) -> io::Result<()> {
-        self.inner.set_only_v6(only_v6)
+        self.listener.set_only_v6(only_v6)
     }
 
     #[allow(deprecated)]
     pub fn only_v6(&self) -> io::Result<bool> {
-        self.inner.only_v6()
+        self.listener.only_v6()
     }
 
     pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
-        self.inner.set_ttl(ttl)
+        self.listener.set_ttl(ttl)
     }
 
     pub fn ttl(&self) -> io::Result<u32> {
-        self.inner.ttl()
+        self.listener.ttl()
     }
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        self.inner.take_error()
+        self.listener.take_error()
     }
 }
 
@@ -228,20 +220,19 @@ impl Evented for TcpListener {
 impl FromRawFd for TcpListener {
     unsafe fn from_raw_fd(fd: RawFd) -> TcpListener {
         TcpListener {
-            inner: net::TcpListener::from_raw_fd(fd),
+            listener: net::TcpListener::from_raw_fd(fd),
         }
     }
 }
 
 impl IntoRawFd for TcpListener {
     fn into_raw_fd(self) -> RawFd {
-        self.inner.into_raw_fd()
+        self.listener.into_raw_fd()
     }
 }
 
 impl AsRawFd for TcpListener {
     fn as_raw_fd(&self) -> RawFd {
-        self.inner.as_raw_fd()
+        self.listener.as_raw_fd()
     }
 }
-
