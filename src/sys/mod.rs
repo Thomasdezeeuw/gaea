9dbad4ccdd5e4b497e00634a2ff07088da1bee32
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-01-19 15:36:31 +0100

    Document the internal sys module

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,3 +1,14 @@
+//! Platform specific types.
+//!
+//! Each platform must have at least the following types:
+//!
+//! * `Events`: system specific events, used in `Selector.select`, wrapped by
+//!   `events::Events`.
+//! * `Selector`: system selector, e.g. `kqueue` or `epoll`, used by `Poll`.
+//! * `TcpStream`: TCP stream, used in the net module.
+//! * `TcpListener`: TCP listener, used in the net module.
+//! * `UdpSocket`: UDP socket, used in the net module.
+
 #[cfg(all(unix, not(target_os = "fuchsia")))]
 mod unix;
 
