ad02abb6b7b11689a76eef9f93965fca0131c18d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-01-22 18:10:06 +0100

    Cleanup src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,61 @@
 //! A fast, low-level IO library for Rust focusing on non-blocking APIs, event
-//! notification, and other useful utilities for building high performance IO
-//! apps.
+//! notification for building high performance I/O apps.
 //!
 //! # Goals
 //!
-//! * Fast - minimal overhead over the equivalent OS facilities (epoll, kqueue, etc...)
-//! * Zero allocations
-//! * A scalable readiness-based API, similar to epoll on Linux
-//! * Design to allow for stack allocated buffers when possible (avoid double buffering).
-//! * Provide utilities such as a timers, a notification channel, buffer abstractions, and a slab.
+//! * Fast - minimal overhead over the equivalent OS facilities (epoll, kqueue, etc.).
+//! * Zero allocations.
+//! * A scalable readiness-based API.
+//! * Provide utilities such as a timers.
 //!
 //! # Usage
 //!
-//! Using mio starts by creating a [`Poll`], which reads events from the OS and
-//! put them into [`Events`]. You can handle IO events from the OS with it.
+//! Using mio starts by creating a [`Poll`], which used to poll events, both
+//! from the OS (backed by epoll, kqueue, etc.) and from user space.
 //!
 //! For more detail, see [`Poll`].
 //!
-//! [`Poll`]: struct.Poll.html
-//! [`Events`]: struct.Events.html
+//! [`Poll`]: poll/struct.Poll.html
 //!
 //! # Example
 //!
 //! ```
-//! use mio::event::{Events, Evented, EventedId};
+//! # use std::error::Error;
+//! # fn try_main() -> Result<(), Box<Error>> {
+//! use mio::event::{Events, EventedId};
 //! use mio::net::{TcpListener, TcpStream};
 //! use mio::poll::{Poll, PollOpt, Ready};
 //!
-//! // Setup some ids to allow us to identify which event is for which socket.
+//! // Unique ids to associate an event with a handle, in this can a TCP
+//! // listener (server) or stream (client).
 //! const SERVER: EventedId = EventedId(0);
 //! const CLIENT: EventedId = EventedId(1);
 //!
-//! let addr = "127.0.0.1:13265".parse().unwrap();
+//! // Setup the server socket.
+//! let addr = "127.0.0.1:12345".parse()?;
+//! let mut server = TcpListener::bind(addr)?;
 //!
-//! // Setup the server socket
-//! let mut server = TcpListener::bind(addr).unwrap();
-//!
-//! // Create a poll instance
-//! let mut poll = Poll::new().unwrap();
-//!
-//! // Start listening for incoming connections
-//! poll.register(&mut server, SERVER, Ready::READABLE, PollOpt::EDGE).unwrap();
-//!
-//! // Setup the client socket
-//! let mut sock = TcpStream::connect(addr).unwrap();
-//!
-//! // Register the socket
-//! poll.register(&mut sock, CLIENT, Ready::READABLE, PollOpt::EDGE).unwrap();
-//!
-//! // Create storage for events
+//! // Create a `Poll` instance along with an events container.
+//! let mut poll = Poll::new()?;
 //! let mut events = Events::with_capacity(1024);
 //!
+//! // Register our TCP listener with `Poll`, this allows us to receive
+//! // notifications about incoming connections.
+//! poll.register(&mut server, SERVER, Ready::READABLE, PollOpt::EDGE)?;
+//!
+//! // Setup the client socket, connection to our server.
+//! let mut sock = TcpStream::connect(addr)?;
+//!
+//! // Register the socket with `Poll`.
+//! poll.register(&mut sock, CLIENT, Ready::READABLE, PollOpt::EDGE)?;
+//!
+//! // Start the event loop.
 //! loop {
-//!     poll.poll(&mut events, None).unwrap();
+//!     // Check for new events.
+//!     poll.poll(&mut events, None)?;
 //!
 //!     for event in &mut events {
+//!         // Depending on the event id we need to take an action.
 //!         match event.id() {
 //!             SERVER => {
 //!                 // Accept and drop the socket immediately, this will close
@@ -70,10 +71,13 @@
 //!         }
 //!     }
 //! }
+//! #     Ok(())
+//! # }
+//! #
+//! # fn main() {
+//! #     try_main().unwrap();
+//! # }
 //! ```
-
-#![doc(html_root_url = "https://docs.rs/mio/0.6.12")]
-#![crate_name = "mio"]
 
 #![warn(missing_debug_implementations,
         missing_docs,
@@ -86,7 +90,6 @@
 
 #[macro_use]
 extern crate bitflags;
-extern crate lazycell;
 #[macro_use]
 extern crate log;
 extern crate net2;
