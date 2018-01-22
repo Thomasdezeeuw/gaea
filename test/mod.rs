c306d78b63b47a328fe4289db1a04cd4013f4c27
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-01-22 21:18:19 +0100

    Drop all tests from the test module
    Code is still there, but it needs to be enable again.

--- test/mod.rs
+++ test/mod.rs
@@ -1,167 +1,49 @@
-#![allow(deprecated)]
-
 extern crate mio;
-extern crate bytes;
-extern crate net2;
 
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate slab;
-extern crate tempdir;
 
-pub use ports::localhost;
-
-mod test_custom_evented;
-mod test_close_on_drop;
-mod test_double_register;
-mod test_echo_server;
-mod test_local_addr_ready;
-mod test_multicast;
-mod test_oneshot;
-mod test_poll;
-mod test_register_deregister;
-mod test_reregister_without_poll;
-mod test_smoke;
-mod test_tcp;
-mod test_tcp_level;
-mod test_udp_level;
-mod test_udp_socket;
-mod test_write_then_drop;
-mod test_timer;
-
-use bytes::{Buf, MutBuf};
-use std::io::{self, Read, Write};
 use std::time::Duration;
-use mio::{Events, Poll};
-use mio::event::Event;
 
-pub trait TryRead {
-    fn try_read_buf<B: MutBuf>(&mut self, buf: &mut B) -> io::Result<Option<usize>>
-        where Self : Sized
-    {
-        // Reads the length of the slice supplied by buf.mut_bytes into the buffer
-        // This is not guaranteed to consume an entire datagram or segment.
-        // If your protocol is msg based (instead of continuous stream) you should
-        // ensure that your buffer is large enough to hold an entire segment (1532 bytes if not jumbo
-        // frames)
-        let res = self.try_read(unsafe { buf.mut_bytes() });
+use mio::poll::Poll;
+use mio::event::{Event, Events};
 
-        if let Ok(Some(cnt)) = res {
-            unsafe { buf.advance(cnt); }
-        }
-
-        res
-    }
-
-    fn try_read(&mut self, buf: &mut [u8]) -> io::Result<Option<usize>>;
+/// Initializate the test setup.
+pub fn init() {
+    // Logger could already be set, so we ignore the result.
+    drop(env_logger::try_init());
 }
 
-pub trait TryWrite {
-    fn try_write_buf<B: Buf>(&mut self, buf: &mut B) -> io::Result<Option<usize>>
-        where Self : Sized
-    {
-        let res = self.try_write(buf.bytes());
-
-        if let Ok(Some(cnt)) = res {
-            buf.advance(cnt);
-        }
-
-        res
-    }
-
-    fn try_write(&mut self, buf: &[u8]) -> io::Result<Option<usize>>;
+/// Initializate the test setup (same as init) and create a Poll instance and
+/// events.
+pub fn init_with_poll(events_capacity: usize) -> (Poll, Events) {
+    init();
+    let poll = Poll::new().expect("unable to create Poll instance");
+    let events = Events::with_capacity(events_capacity, events_capacity);
+    (poll, events)
 }
 
-impl<T: Read> TryRead for T {
-    fn try_read(&mut self, dst: &mut [u8]) -> io::Result<Option<usize>> {
-        self.read(dst).map_non_block()
-    }
-}
-
-impl<T: Write> TryWrite for T {
-    fn try_write(&mut self, src: &[u8]) -> io::Result<Option<usize>> {
-        self.write(src).map_non_block()
-    }
-}
-
-/*
- *
- * ===== Helpers =====
- *
- */
-
-/// A helper trait to provide the map_non_block function on Results.
-trait MapNonBlock<T> {
-    /// Maps a `Result<T>` to a `Result<Option<T>>` by converting
-    /// operation-would-block errors into `Ok(None)`.
-    fn map_non_block(self) -> io::Result<Option<T>>;
-}
-
-impl<T> MapNonBlock<T> for io::Result<T> {
-    fn map_non_block(self) -> io::Result<Option<T>> {
-        use std::io::ErrorKind::WouldBlock;
-
-        match self {
-            Ok(value) => Ok(Some(value)),
-            Err(err) => {
-                if let WouldBlock = err.kind() {
-                    Ok(None)
-                } else {
-                    Err(err)
-                }
-            }
-        }
-    }
-}
-
-mod ports {
-    use std::net::SocketAddr;
-    use std::str::FromStr;
-    use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT};
-    use std::sync::atomic::Ordering::SeqCst;
-
-    // Helper for getting a unique port for the task run
-    // TODO: Reuse ports to not spam the system
-    static mut NEXT_PORT: AtomicUsize = ATOMIC_USIZE_INIT;
-    const FIRST_PORT: usize = 18080;
-
-    fn next_port() -> usize {
-        unsafe {
-            // If the atomic was never used, set it to the initial port
-            NEXT_PORT.compare_and_swap(0, FIRST_PORT, SeqCst);
-
-            // Get and increment the port list
-            NEXT_PORT.fetch_add(1, SeqCst)
-        }
-    }
-
-    pub fn localhost() -> SocketAddr {
-        let s = format!("127.0.0.1:{}", next_port());
-        FromStr::from_str(&s).unwrap()
-    }
-}
-
+/// Poll `poll` and compare the retrieved events with the `expected` ones.
 pub fn expect_events(poll: &mut Poll, events: &mut Events, poll_try_count: usize, mut expected: Vec<Event>) {
-    const MS: u64 = 1_000;
+    let timeout = Duration::from_millis(1_000);
 
     for _ in 0..poll_try_count {
-        poll.poll(events, Some(Duration::from_millis(MS))).unwrap();
+        poll.poll(events, Some(timeout)).expect("unable to poll");
+
         for event in &mut *events {
-            let pos_opt = match expected.iter().position(|exp_event| {
-                (event.token() == exp_event.token()) &&
-                event.readiness().contains(exp_event.readiness())
-            }) {
-                Some(x) => Some(x),
-                None => None,
-            };
-            if let Some(pos) = pos_opt { expected.remove(pos); }
+            let pos = expected.iter()
+                .position(|exp_event| *exp_event == event);
+
+            if let Some(pos) = pos {
+                drop(expected.remove(pos));
+            }
         }
 
-        if expected.len() == 0 {
+        if expected.is_empty() {
             break;
         }
     }
 
-    assert!(expected.len() == 0, "The following expected events were not found: {:?}", expected);
+    assert!(expected.is_empty(), "The following expected events were not found: {:?}", expected);
 }
