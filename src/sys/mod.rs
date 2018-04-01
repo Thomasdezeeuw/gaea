b457fa2bad99c207319ccf4802968d92f98963ba
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-04-01 02:33:06 +0200

    Remove outdated reference to sys::Events

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -2,8 +2,6 @@
 //!
 //! Each platform must have at least the following types:
 //!
-//! * `Events`: system specific events, used in `Selector.select`, wrapped by
-//!   `events::Events`.
 //! * `Selector`: system selector, e.g. `kqueue` or `epoll`, used by `Poll`.
 //! * `TcpStream`: TCP stream, used in the net module.
 //! * `TcpListener`: TCP listener, used in the net module.
