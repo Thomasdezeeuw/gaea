de92eb583efc0cbe96589d1ff76488fa43ab1240
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-08-11 20:41:17 +0200

    Change user space tests to use Poller.notify directly

--- test/userspace_registration.rs
+++ test/userspace_registration.rs
@@ -1,82 +1,18 @@
 use mio_st::event::{Event, EventedId, Ready};
-use mio_st::poll::PollOption;
-use mio_st::registration::{NotifyError, Registration, RegistrationGone};
 
 use {expect_events, init_with_poll};
 
 #[test]
-fn no_notifications_before_registering() {
-    let (mut poll, mut events) = init_with_poll();
-    let (mut _registration, mut notifier) = Registration::new();
-
-    // Not notifications with registering first.
-    assert_eq!(notifier.notify(Ready::WRITABLE), Err(NotifyError::NotRegistered));
-
-    expect_events(&mut poll, &mut events, 1, vec![]);
-}
-
-#[test]
-fn incorrect_readiness() {
-    let (mut poll, mut events) = init_with_poll();
-    let (mut registration, mut notifier) = Registration::new();
-
-    poll.register(&mut registration, EventedId(0), Ready::READABLE, PollOption::Edge).unwrap();
-
-    // Incorrect readiness.
-    assert_eq!(notifier.notify(Ready::empty()), Err(NotifyError::EmptyReadiness));
-    assert_eq!(notifier.notify(Ready::WRITABLE), Err(NotifyError::NoInterest));
-
-    expect_events(&mut poll, &mut events, 1, vec![]);
-}
-
-#[test]
-fn no_notifications_after_registration_dropped() {
-    let (mut poll, mut events) = init_with_poll();
-    let (registration, mut notifier) = Registration::new();
-
-    // No more notifications after it's dropped.
-    drop(registration);
-    assert_eq!(notifier.notify(Ready::READABLE), Err(NotifyError::RegistrationGone));
-
-    expect_events(&mut poll, &mut events, 1, vec![]);
-}
-
-#[test]
-fn notifier_interests() {
-    let (mut poll, mut events) = init_with_poll();
-    let (mut registration, mut notifier) = Registration::new();
-
-    // Before registering this should be empty.
-    assert_eq!(notifier.interests(), Ok(Ready::empty()));
-
-    // After registering it should be an actual value.
-    poll.register(&mut registration, EventedId(0), Ready::WRITABLE, PollOption::Edge).unwrap();
-    assert_eq!(notifier.interests(), Ok(Ready::WRITABLE));
-
-    // No point in getting interests after the registration is dropped.
-    drop(registration);
-    assert_eq!(notifier.interests(), Err(RegistrationGone));
-
-    expect_events(&mut poll, &mut events, 1, vec![]);
-}
-
-#[test]
 fn notify() {
     let (mut poll, mut events) = init_with_poll();
-    let (mut registration, mut notifier) = Registration::new();
 
-    poll.register(&mut registration, EventedId(0), Ready::READABLE, PollOption::Edge).unwrap();
-
-    // Ok.
-    notifier.notify(Ready::READABLE).unwrap();
-
-    // Should become only readable.
-    notifier.notify(Ready::READABLE | Ready::WRITABLE).unwrap();
-    notifier.notify(Ready::all()).unwrap();
+    poll.notify(EventedId(0), Ready::READABLE).unwrap();
+    poll.notify(EventedId(0), Ready::WRITABLE).unwrap();
+    poll.notify(EventedId(0), Ready::READABLE | Ready::WRITABLE).unwrap();
 
     expect_events(&mut poll, &mut events, 1, vec![
         Event::new(EventedId(0), Ready::READABLE),
-        Event::new(EventedId(0), Ready::READABLE),
-        Event::new(EventedId(0), Ready::READABLE),
+        Event::new(EventedId(0), Ready::WRITABLE),
+        Event::new(EventedId(0), Ready::READABLE | Ready::WRITABLE),
     ]);
 }
