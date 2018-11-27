96c5a5b867daa155e8e06d62758b05e012198230
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-11-27 19:08:41 +0100

    Use custom pretty Debug implementation for Interests

--- src/poll/interests.rs
+++ src/poll/interests.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::num::NonZeroU8;
 use std::ops::BitOr;
 
@@ -13,7 +14,7 @@ use std::ops::BitOr;
 /// [`Poller`]: struct.Poller.html
 /// [readable]: #associatedconstant.READABLE
 /// [`poll`]: struct.Poller.html#method.poll
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct Interests(NonZeroU8);
 
@@ -49,5 +50,28 @@ impl BitOr for Interests {
 
     fn bitor(self, rhs: Self) -> Self {
         Interests(unsafe { NonZeroU8::new_unchecked(self.0.get() | rhs.0.get()) })
+    }
+}
+
+impl fmt::Debug for Interests {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.pad(match (self.is_readable(), self.is_writable()) {
+            (true, true) => "READABLE | WRITABLE",
+            (true, false) => "READABLE",
+            (false, true) => "WRITABLE",
+            (false, false) => unreachable!(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::poll::Interests;
+
+    #[test]
+    fn fmt_debug() {
+        assert_eq!(format!("{:?}", Interests::READABLE), "READABLE");
+        assert_eq!(format!("{:?}", Interests::WRITABLE), "WRITABLE");
+        assert_eq!(format!("{:?}", Interests::BOTH), "READABLE | WRITABLE");
     }
 }
