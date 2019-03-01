21d7d69ea34f685900753b47450b081ebb3de9ed
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-03-01 19:10:54 +0100

    Add a test for determing the timeout in poll

---
+++ tests/poll.rs
@@ -0,0 +1,68 @@
+use std::io;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+use mio_st::{Events, poll};
+use mio_st::event::{BlockingSource, Source};
+
+mod util;
+
+use self::util::{TIMEOUT_MARGIN, init};
+
+struct SleepySource;
+
+impl<Evts> Source<Evts> for SleepySource
+    where Evts: Events,
+{
+    fn next_event_available(&self) -> Option<Duration> {
+        None
+    }
+
+    fn poll(&mut self, events: &mut Evts) -> io::Result<()> {
+        self.blocking_poll(events, Some(Duration::from_millis(0)))
+    }
+}
+
+impl<Evts> BlockingSource<Evts> for SleepySource
+    where Evts: Events,
+{
+    fn blocking_poll(&mut self, _events: &mut Evts, timeout: Option<Duration>) -> io::Result<()> {
+        let timeout = timeout.expect("SleepySource needs a timeout");
+        sleep(timeout);
+        Ok(())
+    }
+}
+
+struct AvailableSource(Duration);
+
+impl<Evts> Source<Evts> for AvailableSource
+    where Evts: Events,
+{
+    fn next_event_available(&self) -> Option<Duration> {
+        Some(self.0)
+    }
+
+    fn poll(&mut self, _events: &mut Evts) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+#[test]
+fn poll_determine_timeout() {
+    init();
+
+    let mut events = Vec::new();
+    let timeout = Duration::from_millis(10);
+
+    let start = Instant::now();
+    poll(&mut SleepySource, &mut [&mut AvailableSource(timeout)], &mut events, None).unwrap();
+    assert!(events.is_empty());
+    let duration = start.elapsed();
+    assert!(duration > timeout && duration < timeout + TIMEOUT_MARGIN, "blocking time incorrect");
+
+    let start = Instant::now();
+    poll(&mut SleepySource, &mut [&mut AvailableSource(Duration::from_secs(1))], &mut events, Some(timeout)).unwrap();
+    assert!(events.is_empty());
+    let duration = start.elapsed();
+    assert!(duration > timeout && duration < timeout + TIMEOUT_MARGIN, "blocking time incorrect");
+}
