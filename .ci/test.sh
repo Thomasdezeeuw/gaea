22f9dcf7b15b97f6428f7134e5cf9fe5711bdfa9
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-04-19 11:46:39 +0200

    Move enabling IPv6 on Travis to the test script

--- .ci/test.sh
+++ .ci/test.sh
@@ -10,6 +10,9 @@ FEATURES=""
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
 	FEATURES="$FEATURES disable_test_deadline"
+elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	# Enable IPv6 on Travis' Linux machines.
+	sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
 fi
 
 cargo test --verbose --features "$FEATURES"
