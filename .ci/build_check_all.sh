1db14d564a1a472432829c43a7f6f3eb28d09e9d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-05-15 15:43:29 +0200

    Enable build check for NetBSD

--- .ci/build_check_all.sh
+++ .ci/build_check_all.sh
@@ -6,5 +6,4 @@ set -eux
 .ci/build_check.sh "x86_64-unknown-freebsd"
 .ci/build_check.sh "x86_64-unknown-linux-gnu"
 .ci/build_check.sh "x86_64-unknown-linux-musl"
-# FIXME: issue #69.
-#.ci/build_check.sh "x86_64-unknown-netbsd"
+.ci/build_check.sh "x86_64-unknown-netbsd"
