b4d2bece913473baaf3edb10fca086570b6126ab
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-05-27 19:52:40 +0200

    Add Android to the check all CI script

--- .ci/build_check_all.sh
+++ .ci/build_check_all.sh
@@ -2,6 +2,7 @@
 
 set -eux
 
+.ci/build_check.sh "aarch64-linux-android"
 .ci/build_check.sh "x86_64-apple-darwin"
 .ci/build_check.sh "x86_64-unknown-freebsd"
 .ci/build_check.sh "x86_64-unknown-linux-gnu"
